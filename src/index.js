a645c1c6b35c3fa31bb896953d1177b37a3df443
sachin <sachinsachin1998@gmail.com>
2020-12-23 11:39:51 +0530

    second, React babel webpack configured

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-(function(){
-    console.log("working")
-}());
+import React from 'react';
+import ReactDom from "react-dom";
+import "index.scss"
+
+let App = ()=>{
+    return <h2>Hello from react</h2>
+}
+
+ReactDom.render(<App/>,
+    document.getElementById("root")
+)
